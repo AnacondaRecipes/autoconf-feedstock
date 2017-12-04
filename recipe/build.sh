c790c73a45034e0410e3ff61a333ae9f9a0066d9
Ray Donnelly <mingw.android@gmail.com>
2017-12-04 08:52:07 -0500

    Set --build and --host (needed for ppc64le to compile)

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,8 @@
 
 ./configure --prefix=${PREFIX}        \
             --libdir=${PREFIX}/lib    \
+            --build=${BUILD}          \
+            --host=${HOST}            \
             PERL='/usr/bin/env perl'
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
